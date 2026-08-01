cd5cfe2495640be936b50c455677a20b05b59b15
MeAsMehran <motamednia2003@gmail.com>
2026-08-02 00:39:28 +0330

    backend: Add favorite ad CRUD operations with add, remove, toggle, and list views

--- backend/auto_market/ads/serializers/favorite_serializer.py
+++ backend/auto_market/ads/serializers/favorite_serializer.py
@@ -1,10 +1,15 @@
 
-
 from rest_framework import serializers
 
+##########################################################
 
-
-class CreateFavoriteCarAdSerializer(serializers.Serializer):
+class AddFavoriteAdSerializer(serializers.Serializer):
     pass
 
+
+'''
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favorites')
+    car        = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='favorited_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+'''
 
